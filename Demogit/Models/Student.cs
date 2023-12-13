fb2139cd6605b4eeacb3bd4782f7cf164e4fca4d
Your Name <you@example.com>
2023-12-13 15:17:27 +0700

    append property ID in file Student.cs

--- Demogit/Models/Student.cs
+++ Demogit/Models/Student.cs
@@ -8,10 +8,7 @@ namespace Demogit.Models
 {
     internal class Student
     {
-<<<<<<< HEAD
+
         public int ID { get; set; }
-=======
-        public int MSSV { get; set; }
->>>>>>> V3
     }
 }
